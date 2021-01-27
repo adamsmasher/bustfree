b8ad83f7b828dff3b398ccf187bf63cf01aa960b
Adam Smith <unfactorable.polynomial@gmail.com>
2021-01-27 13:22:31 -0800

    clear out bg map and tiles

--- breakout.asm
+++ breakout.asm
@@ -20,4 +20,13 @@ InitInterrupts: LD A, 1         ; enable vblank
 InitScreen: HALT                    ; wait for vblank
             XOR A                   ; turn the screen off
             LDH [$40], A
+            ; clear tiles and map
+            XOR A
+            LD BC, $1000 + $400
+            LD HL, $8800
+.loop       LD [HLI], A
+            DEC C
+            JR NZ, .loop
+            DEC B
+            JR NZ, .loop
             RET
