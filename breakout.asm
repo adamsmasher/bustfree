77cd4ec2a1a167bd50e4170fc8a92564e4eeef5c
Adam Smith <unfactorable.polynomial@gmail.com>
2021-01-27 13:02:15 -0800

    turn off screen

--- breakout.asm
+++ breakout.asm
@@ -9,8 +9,14 @@ Main:   DI
         LD SP, $E000
         CALL InitInterrupts
         EI
+        CALL InitScreen
 .loop   JR .loop
 
 InitInterrupts: LD A, 1         ; enable vblank
                 LDH [$FF], A
                 RET
+
+InitScreen: HALT                    ; wait for vblank
+            XOR A                   ; turn the screen off
+            LDH [$40], A
+            RET
