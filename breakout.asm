bcfea28456146e687ed9e2690f37f06fe62fa176
Adam Smith <unfactorable.polynomial@gmail.com>
2021-01-27 13:02:03 -0800

    setup interrupts

--- breakout.asm
+++ breakout.asm
@@ -1,5 +1,16 @@
 SECTION "Boot", ROM0[$0100]
 Boot:   JP Main
 
+SECTION "VBlank", ROM0[$0040]
+RETI
+
 SECTION "Main", ROM0
-Main:   JR Main
+Main:   DI
+        LD SP, $E000
+        CALL InitInterrupts
+        EI
+.loop   JR .loop
+
+InitInterrupts: LD A, 1         ; enable vblank
+                LDH [$FF], A
+                RET
