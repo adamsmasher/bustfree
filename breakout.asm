b0307b19416642eff33888bd8197b6751abb3742
Adam Smith <unfactorable.polynomial@gmail.com>
2021-01-27 14:04:56 -0800

    OAM

--- breakout.asm
+++ breakout.asm
@@ -1,13 +1,42 @@
+SECTION "ShadowOAM", WRAM0, ALIGN[8]
+ShadowOAM: DS 4 * 40
+
 SECTION "Boot", ROM0[$0100]
 Boot:   JP Main
 
-SECTION "VBlank", ROM0[$0040]
-RETI
+SECTION "VBlankInt", ROM0[$0040]
+JP VBlank
+
+SECTION "VBlank", ROM0
+VBlank: PUSH AF
+        PUSH BC
+        PUSH DE
+        PUSH HL
+        CALL OAMDMA
+        POP HL
+        POP DE
+        POP BC
+        POP AF
+        RETI
+
+SECTION "OAMDMA", ROM0
+
+OAMDMACode:
+LOAD "OAMDMA code", HRAM
+OAMDMA: LD A, HIGH(ShadowOAM)
+        LDH [$46], A
+        LD A, $28
+.wait   DEC A
+        JR NZ, .wait
+        RET
+.end
+ENDL
 
 SECTION "Main", ROM0
 Main:   DI
         LD SP, $E000
         CALL InitInterrupts
+        CALL InitShadowOAM
         EI
         CALL InitScreen
 .loop   HALT
@@ -35,3 +64,21 @@ InitScreen: HALT                    ; wait for vblank
             LD A, %10000000
             LDH [$40], A
             RET
+
+InitShadowOAM:  ; clear shadow OAM
+                XOR A
+                LD HL, ShadowOAM
+                LD B, 4 * 40
+.shadowOAM      LD [HLI], A
+                DEC B
+                JR NZ, .shadowOAM
+                ; copy the OAMDMA routine
+                LD HL, OAMDMACode
+                LD DE, OAMDMA
+                LD B, OAMDMA.end - OAMDMA
+.oamdma         LD A, [HLI]
+                LD [DE], A
+                INC E
+                DEC B
+                JR NZ, .oamdma
+                RET
