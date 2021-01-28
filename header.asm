8999f61d788fd47482ad118183ae5297ac218b4a
Adam Smith <unfactorable.polynomial@gmail.com>
2021-01-28 15:30:09 -0800

    reserve header space so the assembler doesn't put code in header

--- header.asm
+++ header.asm
@@ -1,7 +1,16 @@
+SECTION "NintendoLogo", ROM0[$0104]
+DS 48, $00
+
 SECTION "Title", ROM0[$0134]
 ;   01234567
 DB "BREAKOUT"
 DS 8, $00
+
+SECTION "NewLicenseeCode", ROM0[$0144]
+DS 2, $00
+
+SECTION "SGBFlag", ROM0[$0146]
+DB $00
 
 SECTION "CartridgeType", ROM0[$0147]
 DB $00  ; ROM only
@@ -14,3 +23,15 @@ DB $00  ; No RAM
 
 SECTION "DestinationCode", ROM0[$014A]
 DB $01  ; non-Japanese
+
+SECTION "OldLicenseeCode", ROM0[$014B]
+DB $00
+
+SECTION "MaskROMVersion", ROM0[$014C]
+DB $00
+
+SECTION "HeaderChecksum", ROM0[$014D]
+DB $00
+
+SECTION "GlobalChecksum", ROM0[$014E]
+DW $0000
