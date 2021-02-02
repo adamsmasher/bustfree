cf3aaaa7ab7b0d8b055fc12cf24bf8bdb50ad255
Adam Smith <unfactorable.polynomial@gmail.com>
2021-02-02 14:23:56 -0800

    move ball code to separate module

--- make.bat
+++ make.bat
@@ -1,8 +1,9 @@
+rgbasm -o ball.o ball.asm
 rgbasm -o breakout.o breakout.asm
 rgbasm -o header.o header.asm
 rgbasm -o input.o input.asm
 rgbasm -o oam.o oam.asm
 rgbasm -o paddle.o paddle.asm
 rgbasm -o sprites.o sprites.asm
-rgblink -o breakout.gb breakout.o header.o input.o oam.o paddle.o sprites.o
+rgblink -o breakout.gb ball.o breakout.o header.o input.o oam.o paddle.o sprites.o
 rgbfix -v -p 0 breakout.gb
