4571d1fbb41a141e540a550a5041ec73cb8bffb4
Adam Smith <unfactorable.polynomial@gmail.com>
2021-02-03 14:57:59 -0800

    add bg

--- breakout.asm
+++ breakout.asm
@@ -4,6 +4,46 @@ Boot:   JP Main
 SECTION "VBlankInt", ROM0[$0040]
 JP VBlank
 
+SECTION "StageRAM", WRAM0, ALIGN[8]
+StageMap::      DS 16 * 8
+
+SECTION "StageData", ROM0
+StageData:
+PUSHC
+CHARMAP ".", 0
+CHARMAP "#", $80
+;   0123456789ABCDEF
+DB "..####..####..#."
+DB "..####..####..#."
+DB "..####..####..#."
+DB "..####..####..#."
+DB "..####..####..#."
+DB "..####..####..#."
+DB "..####..####..#."
+DB "..####..####..#."
+POPC
+
+SECTION "BGTileData", ROM0
+BGTileData:
+DW `11111111
+DW `12333321
+DW `12333321
+DW `12333321
+DW `12333321
+DW `12333321
+DW `12333321
+DW `11111111
+
+LoadBGGfx:      LD HL, BGTileData
+                LD DE, $8800
+                LD B, 16
+.loop           LD A, [HLI]
+                LD [DE], A
+                INC DE
+                DEC B
+                JR NZ, .loop
+                RET
+
 SECTION "VBlank", ROM0
 VBlank: PUSH AF
         PUSH BC
@@ -25,10 +65,13 @@ Main:   DI
         CALL TurnOffScreen
         CALL ClearVRAM
         CALL InitPalette
+        CALL LoadBGGfx
         CALL LoadSpriteGfx
         CALL InitBall
-        Call InitPaddle
+        CALL InitPaddle
+        CALL InitStage
         CALL InitInput
+        CALL DrawStage
         CALL TurnOnScreen
 .loop   CALL UpdateInput
         CALL UpdateBall
@@ -58,6 +101,7 @@ ClearVRAM:      XOR A
                 RET
 
 InitPalette:    LD A, %11100100
+                LDH [$47], A
                 LDH [$48], A
                 RET
 
@@ -65,6 +109,38 @@ TurnOnScreen:   ; enable display
                 ; BG tiles at $8800
                 ; map at $9800
                 ; sprites enabled
-                LD A, %10000010
+                ; bg enabled
+                LD A, %10000011
                 LDH [$40], A
                 RET
+
+InitStage:      LD HL, StageData
+                LD DE, StageMap
+                LD B, 128
+.loop           LD A, [HLI]
+                LD [DE], A
+                INC E
+                DEC B
+                JR NZ, .loop
+                RET
+
+DrawStage:      LD HL, StageMap
+                LD DE, $9842
+                LD B, 8
+.drawRow        PUSH DE
+                LD C, 16
+.drawCol        LD A, [HLI]
+                LD [DE], A
+                INC E
+                DEC C
+                JR NZ, .drawCol
+                POP DE
+                LD A, E
+                ADD $20
+                LD E, A
+                JR NC, .nc
+                INC D
+.nc             DEC B
+                JR NZ, .drawRow
+                RET
+
