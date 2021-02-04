d422afd134fa09c79c7d15906ec227f5ccb8100a
Adam Smith <unfactorable.polynomial@gmail.com>
2021-02-04 14:49:36 -0800

    generate map and symbol files

--- make.bat
+++ make.bat
@@ -5,5 +5,5 @@ rgbasm -o input.o input.asm
 rgbasm -o oam.o oam.asm
 rgbasm -o paddle.o paddle.asm
 rgbasm -o sprites.o sprites.asm
-rgblink -o breakout.gb ball.o breakout.o header.o input.o oam.o paddle.o sprites.o
+rgblink -d -m breakout.map -n breakout.sym -o breakout.gb ball.o breakout.o header.o input.o oam.o paddle.o sprites.o
 rgbfix -v -p 0 breakout.gb
