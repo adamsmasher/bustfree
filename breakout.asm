fed1d1bf9eab6dea7e3659702246b91431abddc5
Adam Smith <unfactorable.polynomial@gmail.com>
2021-02-04 14:50:47 -0800

    add code to update vram

--- breakout.asm
+++ breakout.asm
@@ -44,22 +44,45 @@ LoadBGGfx:      LD HL, BGTileData
                 JR NZ, .loop
                 RET
 
+SECTION "VBlankRAM", WRAM0
+VRAMUpdateNeeded::      DS 1
+VRAMUpdateData::        DS 1
+VRAMUpdateAddr::        DS 2
+
 SECTION "VBlank", ROM0
 VBlank: PUSH AF
         PUSH BC
         PUSH DE
         PUSH HL
+        LD A, [VRAMUpdateNeeded]
+        AND A
+        CALL NZ, RunVBlankUpdate
         CALL OAMDMA
+        XOR A
+        LD [VRAMUpdateNeeded], A
         POP HL
         POP DE
         POP BC
         POP AF
         RETI
 
+InitVBlank:     XOR A
+                LD [VRAMUpdateNeeded], A
+                RET
+
+RunVBlankUpdate:        LD HL, VRAMUpdateAddr
+                        LD A, [HLI]
+                        LD H, [HL]
+                        LD L, A
+                        LD A, [VRAMUpdateData]
+                        LD [HL], A
+                        RET
+
 SECTION "Main", ROM0
 Main:   DI
         LD SP, $E000
         CALL InitInterrupts
+        CALL InitVBlank
         CALL InitShadowOAM
         EI
         CALL TurnOffScreen
