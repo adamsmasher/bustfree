bc76ec2b579b8df1a7b21acc99c2dc12a33431b0
Adam Smith <unfactorable.polynomial@gmail.com>
2021-02-05 14:13:13 -0800

    move vblank into its own module

--- breakout.asm
+++ breakout.asm
@@ -1,9 +1,6 @@
 SECTION "Boot", ROM0[$0100]
 Boot:   JP Main
 
-SECTION "VBlankInt", ROM0[$0040]
-JP VBlank
-
 SECTION "StageRAM", WRAM0, ALIGN[8]
 StageMap::      DS 16 * 8
 
@@ -43,40 +40,6 @@ LoadBGGfx:      LD HL, BGTileData
                 DEC B
                 JR NZ, .loop
                 RET
-
-SECTION "VBlankRAM", WRAM0
-VRAMUpdateNeeded::      DS 1
-VRAMUpdateData::        DS 1
-VRAMUpdateAddr::        DS 2
-
-SECTION "VBlank", ROM0
-VBlank: PUSH AF
-        PUSH BC
-        PUSH DE
-        PUSH HL
-        LD A, [VRAMUpdateNeeded]
-        AND A
-        CALL NZ, RunVBlankUpdate
-        CALL OAMDMA
-        XOR A
-        LD [VRAMUpdateNeeded], A
-        POP HL
-        POP DE
-        POP BC
-        POP AF
-        RETI
-
-InitVBlank:     XOR A
-                LD [VRAMUpdateNeeded], A
-                RET
-
-RunVBlankUpdate:        LD HL, VRAMUpdateAddr
-                        LD A, [HLI]
-                        LD H, [HL]
-                        LD L, A
-                        LD A, [VRAMUpdateData]
-                        LD [HL], A
-                        RET
 
 SECTION "Main", ROM0
 Main:   DI
@@ -166,4 +129,3 @@ DrawStage:      LD HL, StageMap
 .nc             DEC B
                 JR NZ, .drawRow
                 RET
-
--- make.bat
+++ make.bat
@@ -5,5 +5,6 @@ rgbasm -o input.o input.asm
 rgbasm -o oam.o oam.asm
 rgbasm -o paddle.o paddle.asm
 rgbasm -o sprites.o sprites.asm
-rgblink -d -m breakout.map -n breakout.sym -o breakout.gb ball.o breakout.o header.o input.o oam.o paddle.o sprites.o
+rgbasm -o vblank.o vblank.asm
+rgblink -d -m breakout.map -n breakout.sym -o breakout.gb ball.o breakout.o header.o input.o oam.o paddle.o sprites.o vblank.o
 rgbfix -v -p 0 breakout.gb
