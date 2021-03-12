44a9e01c50c653e3c20bcd4aa5b33dc0f1613acf
Adam Smith <unfactorable.polynomial@gmail.com>
2021-03-12 14:09:38 -0800

    only open file once ready to write

--- tiled/bustfree.py
+++ tiled/bustfree.py
@@ -10,20 +10,29 @@ class Example(Plugin):
         return "bustfree"
 
     @classmethod
+    def getRowTiles(cls, layer):
+        assert layer.width() == 16
+        assert layer.height() == 16
+        for y in range(layer.height() // 2):
+            rowTiles = []
+            for x in range(layer.width()):
+                top = layer.cellAt(x, y * 2).tile()
+                bottom = layer.cellAt(x, y * 2 + 1).tile()
+                assert top
+                assert bottom
+                rowTiles.append(top.id() << 4 | bottom.id())
+            yield rowTiles
+
+    @classmethod
     def write(cls, tileMap, fileName):
+        assert tileMap.tileLayerCount() == 1
+
+        for i in range(tileMap.layerCount()):
+            if isTileLayerAt(tileMap, i):
+                rowTiles = cls.getRowTiles(tileLayerAt(tileMap, i))
+
         with open(fileName, 'wb') as fileHandle:
-            assert tileMap.layerCount() == 1
-            layer = tileLayerAt(tileMap, 0)
-            assert layer.width() == 16
-            assert layer.height() == 16
-            for y in range(layer.height() // 2):
-                rowTiles = []
-                for x in range(layer.width()):
-                    top = layer.cellAt(x, y * 2).tile()
-                    bottom = layer.cellAt(x, y * 2 + 1).tile()
-                    assert top
-                    assert bottom
-                    rowTiles.append(top.id() << 4 | bottom.id())
-                fileHandle.write(bytes(rowTiles))
+            for row in rowTiles:
+                fileHandle.write(bytes(row))
 
         return True
